a2d0da9de2fff248b63181d3234ee5a782bb7268
Chandler-Noe <69088398+Chandler-Noe@users.noreply.github.com>
2020-08-01 16:43:25 -0400

    Quick Update
    - Added file opening and closing
    - Added some variables and output

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Program that gives you a stock to YOLO your life savings on.
 
 Function:
 Stock tickers are taken from a masterlist given by the NASDAQ and picked at random.
+Picks random option strategy.
 
 Planned Updates:
 Automatically update the lists every night from the NASDAQ website.
 Pick random strike prices.
 Pick random expirys.
-Pick random option strategy.
--- stocks.c
+++ stocks.c
@@ -1,16 +1,52 @@
-/* Chandler Noe, August 1, 2020 */
+/* Chandler Noe, 2020 */
 
 /* ----------STOCK YOLO---------- */
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 int main(void) {
 
+  FILE *fp;
+  char *ticker;
+  char *name;
+  char etf[1];
+  char type[13][64] = {
+                    "Call",
+                    "Put",
+                    "Call Credit Spread",
+                    "Call Debit Spread",
+                    "Put Credit Spread",
+                    "Put Debit Spread",
+                    "Iron Condor",
+                    "Butterfly",
+                    "Diagonal Spread",
+                    "Double Diagonal Spread",
+                    "Straddle",
+                    "Strangle",
+                    "Box Spread"
+  };
+
+  printf("Welcome, here is a random stock to YOLO.\n\n");
+
+  fp = fopen("NasdaqListed.txt","r");
+
+  if (fp ==NULL) {
+    fprintf(stderr,"Error! Cant open file!\n");
+    exit(1);
+  }
 
 
+  fclose(fp);
 
+  //Printing information
+  printf("Ticker: %s\n",ticker);
+  printf("Name: %s\n",name);
+  printf("ETF: %s\n\n",etf);
+  printf("Option Type: %s\n",type);
 
   return 0;
 }
